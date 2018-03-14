951e090379184fd09521dab83384e32d70e8d5d0
akbarghi <akbarghifaray@gmail.com>
2018-03-14 12:11:00 +0700

    Create testis.java

---
+++ testis.java
@@ -0,0 +1,9 @@
+//testis.java
+
+public class Main{
+  public static void main (String [] args){
+    System.out.println("mari kita tes dengan di tis");
+    System.out.println("diatas adaalah abay");
+
+  }
+}
