605e43ddb49aa8d3db951e1c23fd8290dc671c88
akbarghi <akbarghifaray@gmail.com>
2018-03-14 12:11:36 +0700

    Delete testis.java

--- testis.java
+++
@@ -1,9 +0,0 @@
-//testis.java
-
-public class Main{
-  public static void main (String [] args){
-    System.out.println("mari kita tes dengan di tis");
-    System.out.println("diatas adaalah abay");
-
-  }
-}
